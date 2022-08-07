c1fdeaf2833a985378a586eaaf3b47401edf4525
Harin2022 <106149834+Harin2022@users.noreply.github.com>
2022-08-07 22:36:40 +0530

    Add files via upload

---
+++ Data/NewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Data
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Pages/Contacts/DI_BasePage.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContactManager.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace ContactManager.Pages.Contacts
+{
+    public class DI_BasePageModel : PageModel
+    {
+        protected ApplicationDbContext Context { get; }
+        protected IAuthorizationService AuthorizationService { get; }
+        protected UserManager<IdentityUser> UserManager { get; }
+
+        public DI_BasePageModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager) : base()
+        {
+            Context = context;
+            UserManager = userManager;
+            AuthorizationService = authorizationService;
+        }
+    }
+}
